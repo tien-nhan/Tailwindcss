0bf66fe24cefc5b625da9bb3e489315cdf0ecac1
tien nhan <tiennhan98ars@gmail.com>
2024-10-17 11:33:14 +0700

    fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,15 @@ import ProductDetails from "./pages/ProductDetails";
 import Cart from "./pages/Cart";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route index element={<ProductDetails />} />
-				<Route path="product" element={<ProductDetails />} />
-				<Route path="cart" element={<Cart />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<ProductDetails />} />
+        <Route path="/product" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
